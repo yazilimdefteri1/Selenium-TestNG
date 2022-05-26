a682d15e1d693590ca0f90c2afbb5b66c382883e
Ayhan Yazilim <yazilimdefteri1@gmail.com>
2022-05-26 23:43:42 +0300

    TestNG calismalarim

---
+++ src/test/java/day16_notations/C03_Priority.java
@@ -0,0 +1,33 @@
+package day16_notations;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C03_Priority extends TestBase {
+
+    /*
+    TestNG test methodlarini isim sirasina gore calistirir.
+    Eger isim siralamasinin disinda bir siralama ile calismasini isterseniz
+    o zaman test methodlarini oncelik(Priority) tanimlayabiliriz.
+
+    priority kucukten buyuge gore calisir
+    eger bir test methoduna priority degeri atanmamissa
+    default olarak priority=0 kabul edilir.
+     */
+
+    @Test (priority = -5)
+    public void amazonTesti(){
+        driver.get("https://www.amazon.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+    @Test
+    public void test02(){
+        driver.get("https://www.bestbuy.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+    @Test
+    public void techproedTesti(){
+        driver.get("https://www.techproeducation.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+}
---
+++ src/test/java/day16_notations/C04_DependsOnMethods.java
@@ -0,0 +1,66 @@
+package day16_notations;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
+import java.time.Duration;
+
+public class C04_DependsOnMethods {
+
+    // extends TestBase Yapmadik
+    // cunku adim adim method olusturdugumuz icin
+    // afterMethodda chrome kapandigi icin diger testler calismayacaktir.
+
+
+    /*
+    DependsOnMethods test methoalarinin calisma siralamasina karisma
+    Sadece bagli olan test, baglandigi testin sonucuna bakar
+    baglandigi test Passed olmazsa, baglanan test hic calismaz(ignore)
+     */
+    WebDriver driver;
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @AfterClass
+    public void tearDown(){
+
+        driver.quit();
+    }
+
+    @Test
+    public void test01(){
+        //amazon anasayfaya gidelim
+        driver.get("https://www.ramazon1.com");
+    }
+
+    @Test (dependsOnMethods="test01",priority = 0)
+    public void test02(){
+        // Nutellaaratalim
+        WebElement aramaKurutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKurutusu.sendKeys("Nutella"+ Keys.ENTER);
+    }
+    @Test (dependsOnMethods="test02")
+    public void test03(){
+        // Sonuc yazisinin Nutella icerdigini test edelim
+
+        WebElement sonucYaziElementi=driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+        Assert.assertTrue(sonucYaziElementi.getText().contains("Nutella"));
+    }
+    @Test
+    public void test04(){
+        System.out.println("Bak bu calsiti");
+    }
+
+}
