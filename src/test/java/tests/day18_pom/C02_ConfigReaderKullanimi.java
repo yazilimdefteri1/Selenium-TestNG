b7dbeb7a90d23d8e9243264026d5ef0c2e6283d7
Ayhan Yazilim <yazilimdefteri1@gmail.com>
2022-05-28 23:22:09 +0300

    27.05.2022

---
+++ src/test/java/tests/day18_pom/C02_ConfigReaderKullanimi.java
@@ -0,0 +1,29 @@
+package tests.day18_pom;
+
+
+import com.github.javafaker.Faker;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.FacebookPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+public class C02_ConfigReaderKullanimi {
+    @Test
+    public void test01() {
+        FacebookPage facebookPage=new FacebookPage();
+        // facebook anasayfaya gidin
+        Driver.getDriver().get(ConfigReader.getProperty("facebookUrl"));
+        // cikarsa cookies kabul edin
+                    // facebookPage.cookieButonu.click();
+        // kullanici mail kutusuna yanlis kullanici ismi yazdirin
+        facebookPage.mailKutusu.sendKeys(ConfigReader.getProperty("fbWrongUserName"));
+        // kullanici sifre kutusuna yanlis password yazdirin
+        facebookPage.passKutusu.sendKeys(ConfigReader.getProperty("fbWrongPassword"));
+        // login butonuna basin
+        facebookPage.loginButonu.click();
+        // giris yapilamadigini test edin
+        Assert.assertTrue(facebookPage.girilemediYaziElementi.isDisplayed());
+        // sayfayi kapatin
+        Driver.closeDriver();
+    }
+}
